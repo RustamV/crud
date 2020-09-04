89eada16bf2406295755bf2f0110c7ce39ff6513
emertarion <rvrd1996@gmail.com>
2020-09-04 21:47:33 +0500

    deploy

---
+++ precache-manifest.fc8f2c2d218fbc885332d56a1e8271bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1b88a513dff930e9615c",
+    "url": "/static/js/main.852734c4.chunk.js"
+  },
+  {
+    "revision": "18679c2a722a252e8f0c",
+    "url": "/static/js/2.f446830a.chunk.js"
+  },
+  {
+    "revision": "1b88a513dff930e9615c",
+    "url": "/static/css/main.eb3aec93.chunk.css"
+  },
+  {
+    "revision": "18679c2a722a252e8f0c",
+    "url": "/static/css/2.19b7aacc.chunk.css"
+  },
+  {
+    "revision": "a3720cdc4d5b2e9f75e417d38ffbaf1d",
+    "url": "/index.html"
+  }
+];
