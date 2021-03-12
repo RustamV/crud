74bdb054fac2c405f9b054ed78eddd0a12a6d48f
RustamV <rvrd1996@gmail.com>
2021-03-13 00:12:00 +0500

    update again

---
+++ precache-manifest.5d092b31f8c2cb1479ad555e3b0cac97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e44e604fc2ff18619ef4",
+    "url": "/static/css/main.2c36a9f4.chunk.css"
+  },
+  {
+    "revision": "e44e604fc2ff18619ef4",
+    "url": "/static/js/main.e09b576e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fdf4189f63673e8fda96",
+    "url": "/static/css/2.1f812d82.chunk.css"
+  },
+  {
+    "revision": "fdf4189f63673e8fda96",
+    "url": "/static/js/2.5b8e6719.chunk.js"
+  },
+  {
+    "revision": "3fb02ad88250e6b5a3dc13be129d367c",
+    "url": "/index.html"
+  }
+];
